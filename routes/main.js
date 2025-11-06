87a4d6ac7270fd21523a7e9625167880428f61fe
Abrar <araha002@gold.ac.uk>
2025-11-06 23:23:33 +0000

    Initial

---
+++ routes/main.js
@@ -0,0 +1,44 @@
+// Create a new router
+const express = require("express");
+const router = express.Router();
+
+var shopData = {shopName: "The Thirsty Student",
+                productCategories:["Beer", "Wine", "Soft Drinks", "Hot Drinks"],
+                shopLocations: ['London', 'Manchester'],
+                shopManagers: ['Bob', 'Jeff']
+};
+
+// Handle the main routes
+router.get("/", (req, res) => {
+    res.render("index.ejs", shopData)
+});
+
+// Handles the 'about' route.
+router.get("/about", (req, res) => {
+    res.render("about.ejs", shopData)
+}); 
+
+// Handles the 'search' route.
+router.get('/search', (req, res) => {
+    res.render('search.ejs', shopData)
+});
+
+// Handles the input received by the form in the 'search' route.
+router.get('/search_result', function (req, res) {
+    // TODO: search in the database
+    res.send('You searched for ' + req.query.search_text + ' in ' + req.query.category + '.');
+});
+
+// Handles the 'register' route.
+router.get("/register", (req,res) => {
+    res.render("register.ejs",  shopData); 
+});
+
+// Handles the input received by the form in the 'register' route. 
+router.post("/registered", (req,res) => { 
+  res.send('Hello '+ req.body.first + ' '+ req.body.last +', you are now registered! We will send an email to you at ' + req.body.email + '.') 
+}); 
+
+
+// Export the router object so index.js can access it
+module.exports = router;
